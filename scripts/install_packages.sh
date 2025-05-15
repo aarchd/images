d3966dc63bbaeca6f79b9fb75b9e83d0befb43bd
Deepak Meena <notwho53@gmail.com>
2025-05-15 14:06:51 +0530

    scripts/install_packages: add --needed --noconfirm flags
    Signed-off-by: Deepak Meena <notwho53@gmail.com>

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 VNDK=$1
 
-pacman -Syyu aarchd-base adaptation-hybris-devtools adaptation-hybris-api${VNDK}-phone 
+pacman -Syyu aarchd-base adaptation-hybris-devtools adaptation-hybris-api${VNDK}-phone --needed --noconfirm
