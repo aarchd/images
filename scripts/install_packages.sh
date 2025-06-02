268b232f7892bd44d6d582d4f95652739a3c1dd4
Deepak Meena <notwho53@gmail.com>
2025-06-02 10:27:01 +0530

    scripts/install_packages: resolve pulseaudio-modules-droid-apispecific dep
    Signed-off-by: Deepak Meena <notwho53@gmail.com>

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -4,4 +4,10 @@ set -euo pipefail
 
 VNDK=$1
 
+if [ $VNDK -gt 29 ]; then
+    pacman -Syyu pulseaudio-modules-droid-modern --needed --noconfirm
+else
+    pacman -Syyu pulseaudio-modules-droid-jb2q --needed --noconfirm
+fi
+
 pacman -Syyu aarchd-base adaptation-hybris-devtools adaptation-hybris-api${VNDK}-phone --needed --noconfirm
